4010e0614ffad3c737d6f39f76959a52d5501037
MyWorldZPe <duy18032003@gmail.com>
2023-11-05 11:26:19 +0700

    update

--- src/services/chatbotServices.js
+++ src/services/chatbotServices.js
@@ -25,7 +25,31 @@ let callSendAPI = (sender_psid, response) => {
         }
     });
 }
-let handleGetStarted = () => {
+
+let getUserName = (sender_psid) => {
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    }
+
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": `https://graph.facebook.com/v18.0/me/messenger_profile?fields=whitelisted_domains,greeting&access_token=<PAGE_ACCESS_TOKEN>`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+}
+let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
             let response = { "text": "Xin chào bạn đã đến với WorldZToy" };
